67d3b28b690b8d8c95ab0676df0f6222608a13f2
Danika Harada <danimi65@gmail.com>
2017-01-21 17:22:33 -1000

    trying to get stuff done

--- js/game.js
+++ js/game.js
@@ -1,131 +1,34 @@
 
 //jshint esversion:6
 
+let speed = 2;
 
-// window.onkeydown = function (e) {
-//   if(!e){
-//     e = window.event;
-//   }
-
-// let score = 0;
-// let key = e.keyCode;
-// let pikachu = document.getElementById("pikachu");
-// let top = parseInt(pikachu.style.top, 10);
-// let left = parseInt(pikachu.style.left, 10);
-
-// if(key === 37 ) {
-//   if(left > 0){
-//     pikachu.style.left = left - 10 + 'px';
-//   }
-// } else if (key == 38){
-//   if(top > 0){
-//     pikachu.style.top = top - 10 + 'px';
-//   }
-// }else if( key === 39 ){
-//   if(left+pikachu.width+10 < window.innerWidth){
-//     pikachu.style.left = left + 10 + 'px';
-//   }
-// }else if (key === 40){
-//   if(top.pikachu.height+ 10 < window.innerHeight){
-//     pikachu.style.top = top + 10 + 'px';
-//   }
-// }
-
-
-
-// };
-var jsdom = require("jsdom").jsdom;
-global.$ = require('jquery/dist/jquery')(jsdom().createWindow());
-
-let keyUp = false;
-let keyRight = false;
-let keyDown = false;
-let gameOver = false;
-
-let move_up = false;
-let move_right = false;
-let move_down = false;
-
-$(document).on('keydown', function(event){
-let score = 0;
-let keyUp = true;
-let keyRight = true;
-let keyDown = true;
-let gameOver = false;
-
-// document.getElementById('pikachu').onkeydown = function(event) {
-  let key = event.keyCode;
-  // up = 38;
-  // right = 39;
-  // down = 40;
-  if(gameOver === false) {
-
-    if(key === 38 && move_up === false) {
-      move_up = requestAnimationFrame(moveUp);
-    } else if( key === 39 && move_right === false) {
-      move_right = requestAnimationFrame(moveRight);
-    } else if(key === 40 && move_down) {
-      move_down = requestAnimationFrame(moveDown);
-
-    // if(key === 38 && keyUp === true) {
-    //   keyUp = requestAnimationFrame(up);
-    // } else if(key === 39 && keyRight === true) {
-    //   keyRight = requestAnimationFrame(right);
-    // } else if(key === 40 && keyDown === true) {
-    //   keyDown = requestAnimationFrame(down);
-
+   $(document).on('keydown', function (e) {
+        // if (game_over === false) {
+    switch (e.which){
+    case 37:    //left arrow key
+        $(".pikachu").finish().animate({
+            left: "-=50"
+        });
+        break;
+    case 38:    //up arrow key
+        $(".pikachu").finish().animate({
+            top: "-=50"
+        });
+        break;
+    case 39:    //right arrow key
+        $(".pikachu").finish().animate({
+            left: "+=50"
+        });
+        break;
+    case 40:    //bottom arrow key
+        $(".pikachu").finish().animate({
+            top: "+=50"
+        });
+        break;
     }
-  }
-});
+        // }
+    });
 
 
-$(document).on('keyup', function(event){
-  let key = event.keyCode;
-
-// document.getElementById('pikachu').onkeyup = function(event) {
-//   let key = event.keyCode;
-  // up = 38;
-  // right = 39;
-  // down = 40;
-  if(gameOver === false) {
-    if(key === 38) {
-      keyUp = cancelAnimationFrame(up);
-    } else if( key === 39) {
-      keyRight = cancelAnimationFrame(right);
-    } else if(key === 40) {
-      keyDown = cancelAnimationFrame(down);
-    }
-  }
-
-});
-
-
-
-
-function moveUp() {
-  if(gameOver === false && parseInt(pikachu.css('top')) > 0) {
-    pikachu.css('top', parseInt(pikachu.css('top')) -3);
-    move_up = requestAnimationFrame(moveUp);
-  }
-}
-
-function moveRight() {
-  if(gameOver === false) {
-    pikachu.css('left', parseInt(pikachu.css('left')) +5);
-    move_right = requestAnimationFrame(moveRight);
-  }
-}
-
-function moveDown() {
-  if(gameOver === false) {
-    keyDown = requestAnimationFrame(moveDown);
-  }
-}
-
-function endGame() {
-  gameOver = true;
-  cancelAnimationFrame(moveUp);
-  cancelAnimationFrame(moveRight);
-  cancelAnimationFrame(moveDown);
-}
 
